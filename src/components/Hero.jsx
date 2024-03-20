e63a4d4b84803bc92257df1d22e26c26d153f0a9
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2024-03-20 18:27:04 +0100

    adding oscurecimiento al abrir el burguer btn

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ProfilePic from '../assets/antonio.png';
+import '../style/hero.css';
+import ContactBtn from './botones/contactButton';
+
+function Hero() {
+  return (
+    <section id="hero" className="hero">
+      <div className="photo-container">
+        <img className="hero-photo" src={ProfilePic} alt="logo_cv" loading='lazy' />
+      </div>
+      <div className="saludo">
+        <div className="name">
+          <h1>Hi, I am <br/>
+          <strong>Antonio</strong></h1>
+        </div>
+        <h2 className="job-title">Frontend Developer</h2>
+        < ContactBtn />
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/botones/contactButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../style/buttons.css';
+
+export default function ContactBtn() {
+  return (
+    <button className="contact-button">Contact</button>
+  );
+}
